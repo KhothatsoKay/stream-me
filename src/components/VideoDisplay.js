db5f3d48ba5f1eccd16494aa5b0bafd41237ddb3
KhothatsoKay <rammzz39@gmail.com>
2024-01-15 13:00:16 +0200

    adjusted height for mobile

--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -1,13 +1,23 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import './VideoDisplay.css';
 import 'popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 const VideoDisplay = ({ videoUrl }) => {
+  const [wrapperHeight, setWrapperHeight] = useState(0);
+
+  useEffect(() => {
+    const setHeight = () => {
+      setWrapperHeight(window.innerHeight);
+    };
+    setHeight();
+    window.addEventListener('resize', setHeight);
+    return () => window.removeEventListener('resize', setHeight);
+  }, []);
+
   return (
-    <div className='player-wrapper'>
+    <div className='player-wrapper' style={{ height: wrapperHeight }}>
       <ReactPlayer
         className='react-player'
         url={videoUrl}
